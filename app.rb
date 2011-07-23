cddf0a2117b65d4f98679b07c8e2eb6b231fd701
Phil Crosby <phil.crosby@gmail.com>
2011-07-23 16:32:39 -0700

    Turn on some error backtraces

--- app.rb
+++ app.rb
@@ -3,8 +3,43 @@
 require "rubygems"
 require "bundler/setup"
 
-require "sinatra"
+require "sinatra/base"
 
-get "/" do
-  "howdy hey"
+class CodeReviewServer < Sinatra::Base
+
+  configure :development do
+    enable :logging
+    set :show_exceptions, false
+    set :dump_errors, false
+
+    error do
+      # Show a more developer-friendly error page and stack traces.
+      content_type "text/plain"
+      error = request.env["sinatra.error"]
+      message = error.message + "\n" + CodeReviewServer.cleanup_backtrace(error.backtrace).join("\n")
+      puts message
+      message
+    end
+  end
+
+  configure :test do
+    set :show_exceptions, false
+    set :dump_errors, false
+  end
+
+  configure :production do
+    enable :logging
+  end
+
+  get "/" do
+    "howdy!"
+  end
+
+  def self.cleanup_backtrace(backtrace_lines)
+    # Don't include the portion of the stacktrace which covers the sinatra intenals. Exclude lines like
+    # /opt/local/lib/ruby/gems/1.8/gems/sinatra-1.2.0/lib/sinatra/base.rb:1125:in `call'
+    stop_at = backtrace_lines.index { |line| line.include?("sinatra") }
+    backtrace_lines[0...stop_at]
+  end
 end
+
