a98860c580f56eb002d1bcd23aadfb5993d8702c
Phil Crosby <phil.crosby@gmail.com>
2011-07-23 17:42:50 -0700

    Use less css to render our css.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source :rubygems
 
 gem "sinatra"
 gem "rerun"
+
+# For invoking the Less CSS compiler, written in javascript, from Ruby.
+# This would be slightly nicer than using the lessc binary directly, but it
+# doesn't seem to be compatible with ruby 1.9.
+# gem "less"
--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ require "bundler/setup"
 require "sinatra/base"
 
 class CodeReviewServer < Sinatra::Base
-
+  # We compile our css using LESS. When in development, only compile it when it has changed.
+  $css_cache = {}
   configure :development do
     enable :logging
     set :show_exceptions, false
@@ -16,7 +17,7 @@ class CodeReviewServer < Sinatra::Base
       # Show a more developer-friendly error page and stack traces.
       content_type "text/plain"
       error = request.env["sinatra.error"]
-      message = error.message + "\n" + CodeReviewServer.cleanup_backtrace(error.backtrace).join("\n")
+      message = error.message + "\n" + cleanup_backtrace(error.backtrace).join("\n")
       puts message
       message
     end
@@ -32,10 +33,29 @@ class CodeReviewServer < Sinatra::Base
   end
 
   get "/" do
-    "howdy!"
+    erb :"index.html"
   end
 
-  def self.cleanup_backtrace(backtrace_lines)
+  # Serve CSS written in the "Less" DSL by first compiling it. We cache the output of the compilation and only
+  # recompile it the source CSS file has changed.
+  get "/css/:filename.css" do
+    next if params[:filename].include?(".")
+    asset_path = "public/#{params[:filename]}.less"
+    # TODO(philc): We should not check the file's md5 more than once when we're running in production mode.
+    md5 = Digest::MD5.hexdigest(File.read(asset_path))
+    cached_asset = $css_cache[asset_path] ||= {}
+    if md5 != cached_asset[:md5]
+      cached_asset[:contents] = compile_less_css(asset_path)
+      cached_asset[:md5] = md5
+    end
+    content_type "text/css", :charset => "utf-8"
+    last_modified File.mtime(asset_path)
+    cached_asset[:contents]
+  end
+
+  def compile_less_css(filename) `lessc #{filename}`.chomp end
+
+  def cleanup_backtrace(backtrace_lines)
     # Don't include the portion of the stacktrace which covers the sinatra intenals. Exclude lines like
     # /opt/local/lib/ruby/gems/1.8/gems/sinatra-1.2.0/lib/sinatra/base.rb:1125:in `call'
     stop_at = backtrace_lines.index { |line| line.include?("sinatra") }
