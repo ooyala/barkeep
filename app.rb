7714fdf60a908e1602d7c45b96e75eeb59e9d964
Kevin Le <solnovus@gmail.com>
2011-07-30 12:14:56 -0700

    moved diff stuff into the git helper file

--- app.rb
+++ app.rb
@@ -115,42 +115,4 @@ class CodeReviewServer < Sinatra::Base
       dataset.first
     end
   end
-
-  def apply_diff(data, diff)
-    data_lines = data.split("\n")
-    diff_lines = diff.split("\n")
-    tagged_lines = []
-    chunk_starts = []
-    diff_lines.each_with_index do |line, index|
-      match = /^@@ \-(\d+),(\d+) \+\d+,\d+ @@$/.match(line)
-      chunk_starts << { :index => index, :line => Integer(match[1]), :length => Integer(match[2]) } if match
-    end
-
-    current_line = 1
-    chunk_starts.each_with_index do |chunk, index|
-      if chunk[:line] > current_line
-        tagged_lines += data_lines[current_line-1...chunk[:line]-1].map do |data|
-          { :tag => :same, :data => data }
-        end
-      end
-      next_chunk_start = chunk_starts[index+1] || diff_lines.count
-      (chunk[:index]+1...next_chunk_start).each do |diff_index|
-        case diff_lines[diff_index][0]
-        when " " then tag = :same
-        when "+" then tag = :added
-        when "-" then tag = :removed
-        end
-        tagged_lines << { :tag => tag, :data => diff_lines[diff_index][1..-1] }
-      end
-      current_line += chunk[:length]
-    end
-    last_chunk = chunk_starts[-1]
-    remaining_line_number = last_chunk[:line] + last_chunk[:length]
-    if remaining_line_number <= data_lines.count
-      tagged_lines += data_lines[remaining_line_number..data_lines.count].map do |data|
-        { :tag => :same, :data => data }
-      end
-    end
-    tagged_lines
-  end
 end
--- lib/git_helper.rb
+++ lib/git_helper.rb
@@ -22,4 +22,46 @@ class GitHelper
     commit.author.email.downcase.index(author_search) == 0 ||
     commit.author.to_s.downcase.index(author_search) == 0
   end
+
+  def self.get_tagged_commit_diffs(commit)
+
+  end
+
+  def self.apply_diff(data, diff)
+    data_lines = data.split("\n")
+    diff_lines = diff.split("\n")
+    tagged_lines = []
+    chunk_starts = []
+    diff_lines.each_with_index do |line, index|
+      match = /^@@ \-(\d+),(\d+) \+\d+,\d+ @@$/.match(line)
+      chunk_starts << { :index => index, :line => Integer(match[1]), :length => Integer(match[2]) } if match
+    end
+
+    current_line = 1
+    chunk_starts.each_with_index do |chunk, index|
+      if chunk[:line] > current_line
+        tagged_lines += data_lines[current_line-1...chunk[:line]-1].map do |data|
+          { :tag => :same, :data => data }
+        end
+      end
+      next_chunk_start = chunk_starts[index+1] || diff_lines.count
+      (chunk[:index]+1...next_chunk_start).each do |diff_index|
+        case diff_lines[diff_index][0]
+        when " " then tag = :same
+        when "+" then tag = :added
+        when "-" then tag = :removed
+        end
+        tagged_lines << { :tag => tag, :data => diff_lines[diff_index][1..-1] }
+      end
+      current_line += chunk[:length]
+    end
+    last_chunk = chunk_starts[-1]
+    remaining_line_number = last_chunk[:line] + last_chunk[:length]
+    if remaining_line_number <= data_lines.count
+      tagged_lines += data_lines[remaining_line_number..data_lines.count].map do |data|
+        { :tag => :same, :data => data }
+      end
+    end
+    tagged_lines
+  end
 end
