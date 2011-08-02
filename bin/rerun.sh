665747055deeb975605a30ae709f48cee736f927
Caleb Spare <cespare@gmail.com>
2011-08-02 15:38:33 -0700

    Remove styles from rerun watch list (using livecss instead).

--- bin/rerun.sh
+++ bin/rerun.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rerun --pattern '{deploy/*,**/*.{rb,js,css,ru,coffee,less}}' -- rackup --port 4567 config.ru
+rerun --pattern '{deploy/*,**/*.{rb,js,ru,coffee}}' -- rackup --port 4567 config.ru
