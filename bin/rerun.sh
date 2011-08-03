7ef5396167fcfbf3b6b46e7846cfe1cd5ee10ef2
Caleb Spare <cespare@gmail.com>
2011-08-02 17:22:27 -0700

    Add erb files to rerun watch list

--- bin/rerun.sh
+++ bin/rerun.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rerun --pattern '{deploy/*,**/*.{rb,js,ru,coffee}}' -- rackup --port 4567 config.ru
+rerun --pattern '{deploy/*,**/*.{rb,js,ru,coffee,erb}}' -- rackup --port 4567 config.ru
