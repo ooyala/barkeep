b37044c812791085d2d4176dd28678e131b760fb
Caleb Spare <cespare@gmail.com>
2011-08-03 21:15:53 -0700

    Filter livecss output in sinatra logging

--- bin/rerun.sh
+++ bin/rerun.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-rerun --pattern '{deploy/*,**/*.{rb,ru}}' -- thin start -p 4567 -R config.ru
+#!/bin/bash
+rerun --pattern '{deploy/*,**/*.{rb,ru}}' -- thin start -p 4567 -R config.ru 2> >(grep -v "cache_bust")
