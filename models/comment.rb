80e0c43c8b2613b96516c9e20b68b7ca8f1d9c3e
Bo Chen <bochen@ooyala.com>
2011-08-04 22:59:09 -0700

    display comments, unstyled

--- models/comment.rb
+++ models/comment.rb
@@ -1,5 +1,8 @@
 #A comment in a commit
 class Comment < Sequel::Model
+  VERSION_BEFORE = "before"
+  VERSION_AFTER = "after"
+
   many_to_one :user
   many_to_one :commit_file
   many_to_one :commit
