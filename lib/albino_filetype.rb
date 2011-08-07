a67862d0aa7ec7f10daca977e053cbb412a299a8
Kevin Le <solnovus@gmail.com>
2011-08-07 03:21:51 -0700

    filetype detection lib

---
+++ lib/albino_filetype.rb
@@ -0,0 +1,20 @@
+class AlbinoFiletype
+  EXTENSION_TO_FILETYPE = {
+    ".rb" => :ruby,
+    "Rakefile" => :ruby,
+    ".erb" => :rhtml,
+    ".xml" => :xml,
+    ".js" => :javascript,
+    ".coffee" => :coffeescript,
+    ".sh" => :bash,
+    ".css" => :css,
+    ".less" => :sass
+  }
+  def self.detect_filetype(filename)
+    # if path, separate file from path
+    filename = filename.include?("/") ? filename[filename.index(%r{/[^/]+$})..-1] : filename
+    extension = filename[filename.index(/\.[^\.]+$/)..-1]
+    filetype = extension ? EXTENSION_TO_FILETYPE[extension] : EXTENSION_TO_FILETYPE[filename]
+    filetype || :text
+  end
+end
