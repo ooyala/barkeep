ef7bb010e5152cb32c7a6b2121ff00b05668475c
Phil Crosby <phil.crosby@gmail.com>
2011-08-09 02:18:01 -0700

    Add a todo to clean up this model.

--- models/commit.rb
+++ models/commit.rb
@@ -1,11 +1,12 @@
-#A single comit
 class Commit < Sequel::Model
   many_to_one :user
   one_to_many :commit_files
   one_to_many :comments
 
-  #list of general comments for this commit
+  # TODO(philc): There should be a way to get a grit_commit from this object.
+
   def commit_comments
-    comments_dataset.filter(:commit_id => id, :commit_file_id => nil, :line_number => nil).order(:created_at).all
+    comments_dataset.filter(:commit_id => id, :commit_file_id => nil, :line_number => nil).
+        order(:created_at).all
   end
 end
