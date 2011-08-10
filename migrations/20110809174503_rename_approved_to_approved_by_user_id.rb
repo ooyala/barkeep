e9fdf8b8fed17fff4527c534eeff835e8725c063
Daniel MacDougall <dmacdougall@gmail.com>
2011-08-09 17:57:02 -0700

    migration to rename 'approved' boolean to 'approved_by_user_id'

---
+++ migrations/20110809174503_rename_approved_to_approved_by_user_id.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    alter_table(:commits) do
+      drop_column :approved
+      add_foreign_key :approved_by_user_id, :users
+    end
+  end
+
+  down do
+    alter_table(:commits) do
+      drop_column :approved_by_user_id
+      add_column :approved, FalseClass, :default => false
+    end
+  end
+end
