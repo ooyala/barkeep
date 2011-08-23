06158d34998391ba10f9b20f560eef4edc3ed2c4
Caleb Spare <cespare@gmail.com>
2011-08-22 20:15:08 -0700

    Changing initial migration to add repo table and fix commit unique constraints.
    Adding new migrations to fix unique constraints wasn't working. This
    should be fine because you have to delete your DB and run migrations
    from scratch after the multi-repo changes anyway.

--- migrations/20110723164631_initial.rb
+++ migrations/20110723164631_initial.rb
@@ -1,8 +1,15 @@
 Sequel.migration do
-  up do
+  change do
+    create_table :git_repos do
+      primary_key :id
+      String :name, :null => false, :unique => true
+      String :path, :null => false, :unique => true
+    end
     create_table(:commits) do
       primary_key :id
-      String :sha, :null => false, :unique => true
+      String :sha, :null => false
+      foreign_key :git_repo_id, :git_repos
+      unique [:git_repo_id, :sha], :name => "sha_is_unique_per_repo"
       text :message
       foreign_key :user_id, :users
       datetime :date
@@ -17,11 +24,5 @@ Sequel.migration do
       String :name
       String :email
     end
-  end
-
-  down do
-    drop_table(:commits)
-    drop_table(:files)
-    drop_table(:users)
   end
 end
