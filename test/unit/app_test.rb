80c6cda12cf5b7a2906d477ba96c159b7ac009ec
Phil Crosby <phil.crosby@gmail.com>
2011-08-24 21:22:28 -0700

    Fix test name

--- test/unit/app_test.rb
+++ test/unit/app_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "../test_helper.rb"))
 require "app"
 
-class EmailsTest < Scope::TestCase
+class AppTest < Scope::TestCase
   include Rack::Test::Methods
 
   def app() Barkeep end
