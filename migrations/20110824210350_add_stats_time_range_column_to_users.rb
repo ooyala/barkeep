c22c69d086d1136fd6318a8835313aa996a27c47
Daniel MacDougall <dmacdougall@gmail.com>
2011-08-24 21:33:02 -0700

    migration to add stats_time_range column to users table

---
+++ migrations/20110824210350_add_stats_time_range_column_to_users.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up do
+		alter_table(:users) do
+			add_column :stats_time_range, String, :default => "month"
+		end
+  end
+
+  down do
+		alter_table(:users) do
+			drop_column :stats_time_range
+		end
+  end
+end
