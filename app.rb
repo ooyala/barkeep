2bc6a95fb5364cf8c03fce6d3311894dcdd3d0c4
Phil Crosby <phil.crosby@gmail.com>
2011-08-30 17:51:09 -0700

    Fix the email preview dev route.

--- app.rb
+++ app.rb
@@ -342,7 +342,7 @@ class Barkeep < Sinatra::Base
   # For development use only -- for testing and styling emails.
   get "/dev/latest_comment_email_preview" do
     comment = Comment.order(:id.desc).first
-    Emails.comment_email_body(comment.grit_commit, [comment])
+    Emails.comment_email_body(comment.commit.grit_commit, [comment])
   end
 
 
