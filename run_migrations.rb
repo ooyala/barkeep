17b066f8e9b474b2987542c1391df072096f1ed2
Phil Crosby <phil.crosby@gmail.com>
2011-09-04 12:52:34 -0700

    Support a rollback param in the "run_migrations" script.

--- run_migrations.rb
+++ run_migrations.rb
@@ -4,9 +4,26 @@ require "./config/environment.rb"
 
 PROTOCOL, DB_TYPE, DB_NAME, DB_HOST = DB_LOCATION.split(":")
 
-case DB_TYPE
-when "Mysql"
-  `sequel -m migrations/ "mysql://#{DB_USER}@#{DB_HOST}/#{DB_NAME}"`
-when "SQLite"
-  `sequel -m migrations/ "sqlite://#{DB_NAME}"`
+migrate_to_version = nil
+if ARGV.include?("rollback")
+  versions = Dir.entries("migrations").map do |filename|
+    # Each migration looks like 1234_name_of_migration.rb.
+    version = filename.match(/(\d+)_.+\.rb/)[1] rescue nil
+  end
+  versions = versions.reject(&:nil?).map(&:to_i).sort
+  migrate_to_version = versions[-2]
 end
+
+command = "sequel -m migrations/"
+command += " -M #{migrate_to_version}" if migrate_to_version
+
+host_string = case DB_TYPE
+when "Mysql"
+  "mysql://#{DB_USER}@#{DB_HOST}/#{DB_NAME}"
+when "SQLite"
+  "sqlite://#{DB_NAME}"
+end
+
+puts "Migrating to version #{migrate_to_version}" if migrate_to_version
+
+puts `#{command} "#{host_string}"`
