f341e6382ab72bbd77cc82dacb37e377d91d7d5b
Phil Crosby <phil.crosby@gmail.com>
2011-09-04 13:49:33 -0700

    Whitespace

--- migrations/migration_helper.rb
+++ migrations/migration_helper.rb
@@ -1,5 +1,3 @@
-# By default, Sequel uses the MyISAM storage engine.
-# Unfortunately, that's slower, more prone to corruption,
+# By default, Sequel uses the MyISAM storage engine. Unfortunately, that's slower, more prone to corruption,
 # has coarser-grain locking and can't do transactions.
-# We prefer InnoDB.
 Sequel::MySQL.default_engine = "InnoDB"
