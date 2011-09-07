0db8f2bf5e19ba7fc789d4c658e3fcbdd3acead9
Caleb Spare <cespare@gmail.com>
2011-09-06 17:36:02 -0700

    Make pretty date intervals more intuitive.
    Also added unit tests for PrettyDate.
    
    fixes #35

--- lib/pretty_date.rb
+++ lib/pretty_date.rb
@@ -1,25 +1,23 @@
 # This module shamelessly copy-pasted from here:
 # http://stackoverflow.com/questions/195740/how-do-you-do-relative-time-in-rails
+# (with adjustments)
 
-module PrettyDate
+class Time
   def to_pretty
-    a = (Time.now-self).to_i
+    a = (Time.now - self).to_i
 
     case a
       when 0 then return 'just now'
       when 1 then return 'a second ago'
       when 2..59 then return a.to_s+' seconds ago'
-      when 60..119 then return 'a minute ago' #120 = 2 minutes
-      when 120..3540 then return (a/60).to_i.to_s+' minutes ago'
-      when 3541..7100 then return 'an hour ago' # 3600 = 1 hour
-      when 7101..82800 then return ((a+99)/3600).to_i.to_s+' hours ago'
-      when 82801..172000 then return 'a day ago' # 86400 = 1 day
-      when 172001..518400 then return ((a+800)/(60*60*24)).to_i.to_s+' days ago'
-      when 518400..1036800 then return strftime("%b %d, %Y")
+      when 60..89 then return 'a minute ago' #90 = 1.5 minutes
+      when 90..3569 then return (a/60.0).round.to_s+' minutes ago'
+      when 3570..5399 then return 'an hour ago' # 3600 = 1 hour
+      when 5400..84599 then return ((a)/3600.0).round.to_s+' hours ago'
+      when 84600..129599 then return 'a day ago'
+      when 129600..561599 then return (a/(60*60*24.0)).round.to_s+' days ago'
+      when 561600..1036800 then return strftime("%b %d, %Y")
     end
     return strftime("%b %d, %Y")
   end
 end
-
-Time.send :include, PrettyDate
-
---
+++ test/unit/pretty_date_test.rb
@@ -0,0 +1,55 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "../test_helper.rb"))
+
+require "lib/pretty_date"
+
+class PrettyDateTest < Scope::TestCase
+  context "to_pretty" do
+    setup do
+      @now = Time.utc(2011, 8, 15, 12, 0) # 2011-08-15 12:00:00 UTC
+      stub(Time).now { @now }
+      @second = 1
+      @minute = 60 * @second
+      @hour = 60 * @minute
+      @day = 24 * @hour
+      @year = 365 * @day
+    end
+
+    should "give reasonable 'seconds ago' representations" do
+      assert_equal "just now", time_ago(0)
+      assert_equal "a second ago", time_ago(1 * @second)
+      assert_equal "30 seconds ago", time_ago(30 * @second)
+      assert_equal "59 seconds ago", time_ago(59 * @second)
+    end
+
+    should "give reasonable 'minutes ago' representations" do
+      assert_equal "a minute ago", time_ago(1 * @minute + 29 * @second)
+      assert_equal "2 minutes ago", time_ago(1 * @minute + 30 * @second)
+      assert_equal "5 minutes ago", time_ago(5 * @minute)
+      assert_equal "59 minutes ago", time_ago(59 * @minute + 29 * @second)
+    end
+
+    should "give reasonable 'hours ago' representations" do
+      assert_equal "an hour ago", time_ago(59 * @minute + 30 * @second)
+      assert_equal "an hour ago", time_ago(1 * @hour + 29 * @minute)
+      assert_equal "2 hours ago", time_ago(1 * @hour + 31 * @minute)
+      assert_equal "10 hours ago", time_ago(10 * @hour)
+      assert_equal "23 hours ago", time_ago(23 * @hour + 29 * @minute)
+    end
+
+    should "give reasonable 'days ago' representations" do
+      assert_equal "a day ago", time_ago(23 * @hour + 30 * @minute)
+      assert_equal "a day ago", time_ago(30 * @hour)
+      assert_equal "2 days ago", time_ago(44 * @hour)
+      assert_equal "6 days ago", time_ago(6 * @day)
+    end
+
+    should "give reasonable representations for times long ago" do
+      assert_equal "Aug 08, 2011", time_ago(7 * @day)
+      assert_equal "Aug 15, 2010", time_ago(1 * @year)
+    end
+  end
+
+  def time_ago(interval)
+    (@now - interval).to_pretty
+  end
+end
