4797e1f63a31da2c97e859e5ccb12b56c932e552
Caleb Spare <cespare@gmail.com>
2011-09-09 09:41:35 -0700

    Added some comments to explain what the Statusz module does

--- lib/statusz.rb
+++ lib/statusz.rb
@@ -1,3 +1,10 @@
+# This reads out some metadata about the current running version of Barkeep from files written at deploy time.
+# In particular, git information such as the current HEAD, current branch, and user info are written to
+# "git_deploy_info.txt", and all the commits reachable from HEAD (that is, the set of all commits contained in the
+# deploy) are written to "all_commits.txt".
+#
+# This information is exposed through the /statusz route.
+
 module Statusz
   def self.summary_info
     File.read File.join(File.dirname(__FILE__), "../git_deploy_info.txt")
