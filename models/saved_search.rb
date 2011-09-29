96c3587f7565d00f45484045bb8b750c638aefff
Caleb Spare <cespare@gmail.com>
2011-09-29 10:52:57 -0700

    Fix confusing saved search title bug.
    Fixes #76.

--- models/saved_search.rb
+++ models/saved_search.rb
@@ -37,7 +37,7 @@ class SavedSearch < Sequel::Model
   # Generates a human readable title based on the search criteria.
   def title
     return "All commits" if [repos, branches, authors, paths, messages].all?(&:nil?)
-    if !repos.nil? && [authors, paths, messages].all?(&:nil?)
+    if !repos.nil? && [authors, branches, paths, messages].all?(&:nil?)
       return "All commits for the #{comma_separated_list(repos_list)} " +
           "#{english_quantity("repo", repos_list.size)}"
     end
