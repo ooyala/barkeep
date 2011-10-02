e4a294f6c489a045470bffc05b655c62569cf987
Phil Crosby <phil.crosby@gmail.com>
2011-10-01 23:56:55 -0700

    Ingest all commits from all remotes when it's the first import. Also, integration tests for fetch_commits. Fixes #94.

--- resque_jobs/fetch_commits.rb
+++ resque_jobs/fetch_commits.rb
@@ -12,19 +12,31 @@ class FetchCommits
   def self.perform
     logger = Logging.logger = Logging.create_logger("fetch_commits.log")
     MetaRepo.logger = logger
+
+    # Reconnect to the database if our connection has timed out.
+    Comment.select(1).first rescue nil
+
     fetch_commits(MetaRepo.instance.repos)
   end
 
   def self.fetch_commits(grit_repos)
     Logging.logger.info "Fetching new commits."
     grit_repos.each do |repo|
-      modified_remotes = fetch_commits_for_repo(repo)
-      Logging.logger.info "Fetched new commits in repo #{repo.name}" unless modified_remotes.empty?
-      modified_remotes.each { |remote| Resque.enqueue(DbCommitIngest, repo.name, remote) }
+      db_repo = GitRepo.first(:name => repo.name)
+      is_first_time_import = db_repo.commits_dataset.first.nil?
+      remotes_to_ingest = fetch_commits_for_repo(repo)
+
+      # We should ingest commits from all remotes (not just the ones that have changed) the first time a
+      # repo is imported.
+      remotes_to_ingest = repo.remotes.map(&:name) if is_first_time_import
+
+      remotes_to_ingest.reject! { |name| name == "origin/HEAD" }
+      Logging.logger.info "Found new commits in repo #{repo.name}." unless remotes_to_ingest.empty?
+      remotes_to_ingest.each { |remote| Resque.enqueue(DbCommitIngest, repo.name, remote) }
     end
   end
 
-  # Returns the names of the remotes which were modified.
+  # Runs git fetch, and returns the names of the remotes which are either new or were modified.
   def self.fetch_commits_for_repo(grit_repo)
     head_of_remote = { }
     grit_repo.remotes.each { |remote| head_of_remote[remote.name] = remote.commit.sha }
@@ -33,7 +45,7 @@ class FetchCommits
 
     # Note: invoking grit_repo.remotes refreshes the remotes, so "remote.commit" will be fresh.
     modified_remotes = grit_repo.remotes.select { |remote| head_of_remote[remote.name] != remote.commit.sha }
-    modified_remotes.map(&:name).reject { |name| name == "origin/HEAD" }
+    modified_remotes.map(&:name)
   end
 end
 
---
+++ test/integration/fetch_commits_integration_test.rb
@@ -0,0 +1,66 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "../integration_test_helper.rb"))
+require "resque_jobs/fetch_commits"
+require "resque_jobs/db_commit_ingest"
+require "test/db_fixtures_helper"
+
+class FetchCommitsIntegrationTest < Scope::TestCase
+  include IntegrationTestHelper
+
+  setup_once do
+    @@db_repo = GitRepo.first(:name => test_repo.name)
+  end
+
+  setup do
+    @enqueued_jobs = []
+    stub(Resque).enqueue { |*args| @enqueued_jobs.push(args) }
+  end
+
+  # We say it's the first import of a repo if it has no commits in the DB yet.
+  context "first-time import" do
+    setup_once do
+      @@db_repo.commits_dataset.destroy
+    end
+
+    should "schedule every remote to be imported when it's the first import" do
+      # Even if none of the remotes has changed, the first import should schedule all commits to be ingested.
+      FetchCommits.perform
+      assert @enqueued_jobs.include?([DbCommitIngest, test_repo.name, "origin/master"]),
+          "No DbCommitIngest was included for the test repo's master branch."
+      assert @enqueued_jobs.include?([DbCommitIngest, test_repo.name, "origin/cheese"]),
+          "No DbCommitIngest was included for the test repo's cheese branch."
+    end
+  end
+
+  # It's not a first time import if we've previously ingested commits for this repo into the DB.
+  context "non-first-time import" do
+    setup_once do
+      head = test_repo.head.commit
+      Commit.filter(:sha => head.sha).destroy
+      @@commit = Commit.create(:sha => head.sha, :message => head.message, :date => head.authored_date,
+        :user_id => integration_test_user.id, :git_repo_id => @@db_repo.id)
+    end
+
+    should "only enqueue a db import for the remotes which have changed" do
+      newer_commit, older_commit = test_repo.commits("origin/cheese", 2)
+      # Modifying this ref file simulates the remote being out of date, so that git fetch can update it.
+      ref_file = File.join(FIXTURES_PATH, TEST_REPO_NAME, ".git/refs/remotes/origin/cheese")
+      begin
+        File.open(ref_file, "w") { |file| file.write(older_commit.sha) }
+        FetchCommits.perform
+        # We should not have enqueued a DB import job for origin/master, since it has no new commits.
+        assert_equal [[DbCommitIngest, test_repo.name, "origin/cheese"]], @enqueued_jobs
+      ensure
+        File.open(ref_file, "w") { |file| file.write(newer_commit.sha) }
+      end
+    end
+
+    should "enqueue no jobs when no remotes have been changed" do
+      FetchCommits.perform
+      assert_equal [], @enqueued_jobs
+    end
+
+    teardown_once do
+      @@commit.destroy
+    end
+  end
+end
