5ef5e9031ba60138ecd1fee043c724bec38fa0b9
Phil Crosby <phil.crosby@gmail.com>
2012-01-28 14:50:57 -0800

    Make the clockwork_jobs script non-daemonized

--- script/clockwork_jobs.rb
+++ script/clockwork_jobs.rb
@@ -1,52 +1,36 @@
 #!/usr/bin/env ruby-local-exec
-# Clockwork is a gem which provides cron-like functionality in Ruby. This script is run as a daemon:
-#   clockwork_jobs.rb start
-#   clockwork_jobs.rb stop
-# When developing and debugging, run it in the foreground, not as a daemon:
-#   clockwork_jobs.rb run
-require "rubygems"
-require "daemons"
+# Clockwork is a gem which provides cron-like functionality in Ruby.
 
-pid_path = File.join(File.dirname(__FILE__), "../tmp")
-log_path = File.join(File.dirname(__FILE__), "../log")
-daemonize_options = {
-  :dir_mode => :script, # Place the pid file relative to this script's directory.
-  :dir => pid_path,
-  :log_dir => log_path,
-  :log_output => true
-}
+require "bundler/setup"
+require "clockwork"
 
-# Note that Daemons changes the current working directory to / when it daemonizes a process (inside run_proc).
+# TODO(philc): Switch to pathological
 project_root = File.expand_path(File.join(File.dirname(__FILE__), "../"))
+$LOAD_PATH.push(project_root) unless $LOAD_PATH.include?(project_root)
+require "resque_jobs/fetch_commits"
+require "resque_jobs/batch_comment_emails"
 
-Daemons.run_proc("clockwork_jobs.rb", daemonize_options) do
-  $LOAD_PATH.push(project_root) unless $LOAD_PATH.include?(project_root)
-  require "clockwork"
-  require "resque_jobs/fetch_commits"
-  require "resque_jobs/batch_comment_emails"
+def clear_resque_queue(queue_name) Resque.redis.del("queue:#{queue_name}") end
 
-  def clear_resque_queue(queue_name) Resque.redis.del("queue:#{queue_name}") end
-
-  # We're enqueing Resque jobs to be performed instead of trying to actually perform the work here from within
-  # the Clockwork process. This is recommended by the Clockwork maintainer. Since Clockwork is a
-  # non-parallelized loop, you don't want to perform long-running blocking work here.
-  # We're clearing out the queue for a job before pushing another item onto its queue, in case the job is
-  # taking a very long time to run. We don't want to build up a backlog on the queue because clockwork is
-  # moving faster than the job is.
-  Clockwork.handler do |job_name|
-    case job_name
-    when "fetch_commits"
-      clear_resque_queue("fetch_commits")
-      Resque.enqueue(FetchCommits)
-    when "batch_comment_emails"
-      clear_resque_queue("batch_comment_emails")
-      Resque.enqueue(BatchCommentEmails)
-    end
+# We're enqueing Resque jobs to be performed instead of trying to actually perform the work here from within
+# the Clockwork process. This is recommended by the Clockwork maintainer. Since Clockwork is a
+# non-parallelized loop, you don't want to perform long-running blocking work here.
+# We're clearing out the queue for a job before pushing another item onto its queue, in case the job is
+# taking a very long time to run. We don't want to build up a backlog on the queue because clockwork is
+# moving faster than the job is.
+Clockwork.handler do |job_name|
+  case job_name
+  when "fetch_commits"
+    clear_resque_queue("fetch_commits")
+    Resque.enqueue(FetchCommits)
+  when "batch_comment_emails"
+    clear_resque_queue("batch_comment_emails")
+    Resque.enqueue(BatchCommentEmails)
   end
-
-  Clockwork.every(45.seconds, "fetch_commits")
-
-  Clockwork.every(10.seconds, "batch_comment_emails")
-
-  Clockwork.run # This is a blocking call.
 end
+
+Clockwork.every(45.seconds, "fetch_commits")
+
+Clockwork.every(10.seconds, "batch_comment_emails")
+
+Clockwork.run # This is a blocking call.
