99b4eaa633dda5d65984083a2ad3dd570c8d23b5
Phil Crosby <phil.crosby@gmail.com>
2012-01-28 15:04:30 -0800

    Remove the removal of cache_bust requests. While useful, this causes stderr to buffer such that it doens't print through foreman.

--- bin/rerun_barkeep.sh
+++ bin/rerun_barkeep.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bundle exec rerun --pattern '{deploy/*,**/*.{rb,ru,txt}}' -- thin start -p 4567 -R config.ru 2> >(grep -v "cache_bust")
+bundle exec rerun --pattern '{deploy/*,**/*.{rb,ru,txt}}' -- thin start -p 4567 -R config.ru
