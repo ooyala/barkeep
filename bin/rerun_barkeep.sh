1008e5c3b9e594647fb907a0ec44d9e1c0cd4e36
Caleb Spare <cespare@gmail.com>
2012-01-28 19:48:40 -0800

    Filter out cache_bust requests (this time with a fix for buffering).

--- bin/rerun_barkeep.sh
+++ bin/rerun_barkeep.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 bundle exec rerun --pattern '{config.ru,lib/**.rb,config/*,*.rb}' -- \
-thin start -p 4567 -R config.ru
+thin start -p 4567 -R config.ru \
+2> >(grep --line-buffered -v "cache_bust")
