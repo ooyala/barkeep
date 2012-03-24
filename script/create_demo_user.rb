69fd576b2442b88882fb1ec270df5053dc4317a2
Phil Crosby <phil.crosby@gmail.com>
2012-03-24 16:39:42 -0700

    Script for creating a demo user

---
+++ script/create_demo_user.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby-local-exec
+# 
+# Inserts a demo user into the database. This demo user is what powers the "read only" demo mode that can
+# be enabled for Barkeep. We use it for the Barkeep demo linked from getbarkeep.com.
+# Most deployments do not want to enable the demo mode, and so they do not want this user.
+
+require "bundler/setup"
+require "pathological"
+require "lib/script_environment"
+
+email = "joedemo@getbarkeep.com"
+unless User.first(:email => email)
+  puts "Creating demo user with email #{email}"
+  User.create(:name => "Joe Demo User", :email => email, :permission => "demo")
+end
