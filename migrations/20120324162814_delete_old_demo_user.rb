bedb7a54fd2b3aa1eb52efcf1d846f4787c60b14
Phil Crosby <phil.crosby@gmail.com>
2012-03-24 16:39:19 -0700

    Delete an old school demo user

---
+++ migrations/20120324162814_delete_old_demo_user.rb
@@ -0,0 +1,11 @@
+require "bundler/setup"
+require "pathological"
+require "migrations/migration_helper.rb"
+
+Sequel.migration do
+  up do
+    # We created this user in the migration "20110730130858_create_a_default_user.rb". We no longer need it
+    # now that we have real user accounts and proper login support.
+    DB[:users].filter(:email => "demo@demo.com").delete
+  end
+end
