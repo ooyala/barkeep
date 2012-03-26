ooyala/barkeep

commit e401cac396ac0e9cbdcd1762121d61a35a3a8200

Use sinatra reloader instead of rerun
Keep a sharp eye -- if this isn't working as well as rerun, let's revert it.

app.rb
@@ -2,6 +2,7 @@ require "bundler/setup"
 require "pathological"
 require "json"
 require "sinatra/base"
+require "sinatra/reloader"
 require "redcarpet"
 require "coffee-script"
 require "nokogiri"
@@ -75,6 +76,12 @@ class Barkeep < Sinatra::Base
       puts message
       message
     end
+
+    register Sinatra::Reloader
+    also_reload "lib/*.rb"
+    also_reload "models/*.rb"
+    also_reload "config/*.rb"
+    also_reload "resque_jobs/*.rb"
   end
 
   configure :test do

Procfile
@@ -1,4 +1,4 @@
 # This is used by the Foreman gem, which we use to start our web server and the background jobs.
-web: bin/rerun_barkeep.sh
+web: bin/run_app.sh
 cron: bundle exec script/run_clockwork.rb
 resque: bundle exec rake resque:work QUEUE=*

README.markdown
@@ -36,7 +36,7 @@ This is how it works on our Mac OS dev laptops; YMMV. First, ensure you've insta
 
 Make sure you're up to date on Ruby gems (`bundle install`) and on migrations (`ruby run_migrations.rb`).
 
-    $ bin/rerun_barkeep.sh # run the server
+    $ bin/run_app.sh # run the server
     # navigate to localhost:4567
 
 These are somewhat optional services to run while developing. These background jobs periodically fetch new

bin/rerun_barkeep.sh
@@ -1,4 +1,0 @@
-#!/bin/bash
-bundle exec rerun --pattern '{config.ru,lib/**.rb,config/*,*.rb}' -- \
-thin start -p 4567 -R config.ru \
-2> >(grep --line-buffered -v "cache_bust")

bin/run_app.sh
@@ -1,8 +1,2 @@
-#!/bin/sh
-# This file will be called to start your application.
-
-
-nohup bundle exec script/run_clockwork.rb > /dev/null 2>&1 &
-nohup bundle exec rake resque:work QUEUE=* > /dev/null 2>&1 &
-RACK_ENV=production nohup bundle exec thin start -p 8081 -R config.ru > /dev/null 2>&1 &
-
+#!/bin/bash
+bundle exec rackup --port ${PORT:-4567} config.ru 2> >(grep --line-buffered -v "bust")

commit 507b10c94ea0aeb0a169d9299633b8cd6be7d4f7

Return nil from non-rendering routes
Apparently sinatra-reloader throws errors when routes return objects
that can't be rendered. The errors started appearing after commit
e401cac396ac0e9cbdcd1762121d61a35a3a8200.

app.rb
@@ -340,6 +340,7 @@ class Barkeep < Sinatra::Base
       search.user_order = searches.index(search.id)
       search.save
     end
+    nil
   end
 
   delete "/saved_searches/:id" do
@@ -355,6 +356,7 @@ class Barkeep < Sinatra::Base
       saved_search.send("#{setting}=", body_params[setting.to_s]) unless body_params[setting.to_s].nil?
     end
     saved_search.save
+    nil
   end
 
   # Handle login complete from openid provider.
@@ -485,6 +487,7 @@ class Barkeep < Sinatra::Base
     commit = Commit.first(:sha => params[:sha])
     emails = params[:emails].split(",").map(&:strip).reject(&:empty?)
     Resque.enqueue(DeliverReviewRequestEmails, commit.git_repo.name, commit.sha, current_user.email, emails)
+    nil
   end
 
   #