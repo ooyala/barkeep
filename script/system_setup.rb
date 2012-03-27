29674c9b4ddc63378b6477429de4cf3c200fd9a2
Caleb Spare <cespare@gmail.com>
2012-03-26 18:36:49 -0700

    Install more dependencies in the Terraform script.

--- script/system_setup.rb
+++ script/system_setup.rb
@@ -5,6 +5,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "terraform_dsl"))
 
 include TerraformDsl
+
 # TODO(caleb): At some point we probably want a strict supported list of Ubuntu versions. (We can check with
 # lsb_release --release)
 unless (`lsb_release --id`["Ubuntu"] rescue nil)
@@ -36,6 +37,56 @@ dep "configure nginx" do
 end
 
 ensure_gem("bundler")
+
+# Compare two version strings (x.y.z -- can be any number of parts) by pairwise comparing the pieces as
+# integers.
+class VersionString
+  include Comparable
+  attr_reader :parts
+  def initialize(s)
+    # It's very common for $COMMAND --version to spit out "my program 1.2.3" or "v1.2.3", so we'll strip
+    # leading non-digits as a simple heuristic.
+    @parts = s.sub(/^\D*/, "").split(".").map(&:to_i)
+  end
+  def <=>(other)
+    @parts.zip(other.parts) do |this, that|
+      return -1 if this < that
+      return 1 if this > that
+    end
+    case
+    when @parts.size > other.parts.size then 1
+    when @parts.size < other.parts.size then -1
+    else 0
+    end
+  end
+  def at_least(min_version_string) self >= VersionString.new(min_version_string) end
+end
+
+# Need a recent (1.7.6+) version of git.
+ensure_ppa("ppa:git-core/ppa")
+dep "git 1.7.6+" do
+  met? { in_path?("git") && VersionString.new(`git --version`).at_least("1.7.6") }
+  meet { install_package("git") }
+end
+
+# Pygments -- python library for syntax coloring
+ensure_package("python-setuptools")
+dep "pip" do
+  met? { in_path? "pip" }
+  meet { shell "easy_install pip" }
+end
+dep "pygments" do
+  met? { in_path? "pygmentize" }
+  meet { shell "pip install pygments" }
+end
+
+# Get a more recent node than the very out-of-date one Ubuntu will install by default (this is necessary for
+# compatibility with some changes to how libraries are handled).
+ensure_ppa("ppa:chris-lea/node.js") # This PPA is endorsed on the node GH wiki
+dep "node.js" do
+  met? { in_path?("node") && VersionString.new(`node --version`).at_least("0.6.0") }
+  meet { install_package("nodejs") }
+end
 
 # Note that this git_ssh_private_key is not checked into the repo. It gets created at deploy time.
 # TODO(philc): Set up the location of this private ssh key outside of the repo
