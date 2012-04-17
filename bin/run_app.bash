80a37f12ea021f25a951c00697f0917062da3710
Phil Crosby <phil.crosby@gmail.com>
2012-04-16 23:25:08 -0700

    Source environment.sh from within unicorn, so it happens inside the upstart daemon in prod.

--- bin/run_app.bash
+++ bin/run_app.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# environment.sh is present after a production deploy, and contains env vars like UNICORN_WORKERS.
+if [ -f environment.sh ]; then
+  source environment.sh
+fi
+
 bundle exec unicorn -c config/unicorn.barkeep.conf 2> >(grep --line-buffered -v "bust")
