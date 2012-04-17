0a29fcc27fac20af9a908c587fa9d376945a4f66
Phil Crosby <phil.crosby@gmail.com>
2012-04-16 23:07:10 -0700

    Delete some old unused deploy recipes

--- config/recipes/coffeescript.rb
+++
@@ -1,8 +0,0 @@
-namespace :fezzik do
-  namespace :coffeescript do
-    remote_task :install do
-      puts "running coffeescript install"
-      run "add-apt-repository ppa:gezakovacs/coffeescript && apt-get update && apt-get install -y coffeescript"
-    end
-  end
-end
