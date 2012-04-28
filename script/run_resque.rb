96c5e9a9398b88c44be69cf7f3642b994fa413da
Caleb Spare <cespare@gmail.com>
2012-04-28 07:00:05 -0700

    Add in missing queue to run script and prevent it from happening again.

--- script/run_resque.rb
+++ script/run_resque.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+
+require "set"
+
 # These queues are listed in priority order, from fast and important to slow and unimportant. This ordering
 # is critical because you don't want unimportant slow jobs to starve the faster jobs. Resque workers will
 # process jobs strictly in this order -- there is no fair round-robin among the queues.
@@ -9,12 +12,18 @@ queue_order = %W(
   db_commit_ingest
   fetch_commits
   clone_new_repo
+  delete_repo
   delete_old_comments_by_demo_users
   deliver_review_request_emails
   deliver_comment_emails
   batch_comment_emails
   deliver_commit_emails
   generate_tagged_diffs
-).join(",")
+)
 
-exec "bash -c 'QUEUE=#{queue_order} bundle exec rake resque:work'"
+resque_job_files = Dir["resque_jobs/*.rb"].map { |file| File.basename(file).sub(/\.rb$/, "") }
+if Set.new(queue_order) != Set.new(resque_job_files)
+  abort "The ordered list of resque queues does not match the files in resque_jobs/!"
+end
+
+exec "bash -c 'QUEUE=#{queue_order.join(",")} bundle exec rake resque:work'"
