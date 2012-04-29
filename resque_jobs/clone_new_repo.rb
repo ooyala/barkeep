9d3490baa9c2d01ff2ee07aa71e969f8925f9654
Phil Crosby <phil.crosby@gmail.com>
2012-04-28 19:14:28 -0700

    Have the clone repos shell out to git.

--- resque_jobs/clone_new_repo.rb
+++ resque_jobs/clone_new_repo.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "pathological"
+require "timeout"
 require "lib/script_environment"
 require "lib/resque_job_helper"
 
@@ -9,7 +10,7 @@ class CloneNewRepo
 
   # We're willing to spend up to 5 minutes to clone a repo. This can be necessary for giant repos or
   # when cloning over a slow network connection.
-  TIMEOUT = 5 * 60
+  CLONE_TIMEOUT = 5 * 60
 
   def self.perform(repo_name, repo_url)
     setup
@@ -22,25 +23,21 @@ class CloneNewRepo
     end
 
     logger.info "Cloning the repo #{repo_url} into #{repo_path}. If it's large, this may take awhile."
+    error = nil
     begin
-      Grit::Git.with_timeout(CloneNewRepo::TIMEOUT) do
-        # If this clone operation requires you to enter a password, it will show a prompt and eventually
-        # time out.
-        Grit::Git.new(repo_path).clone({}, repo_url, repo_path)
-      end
-
-      # Unfortunately, Grit::Git.new(...).clone() will not throw an exception if the repo_url was invalid.
-      # It will just... do nothing.
-      # TODO(caleb) We should be able to clone an empty repo; distinguish that case from an invalid one.
-      unless File.exists?(repo_path) && (Grit::Git.new(repo_path).has_refs? rescue false)
-        raise "Unable to clone repo. Perhaps the repo at #{repo_url} is invalid, empty, or unreachable?"
+      # Shell out instead of using Grit::Git.new(repo_path).clone({}, repo_url, repo_path) because
+      # Grit doesn't raise an exception when it has trouble cloning a repo, so there's no good error feedback.
+      Timeout::timeout(CLONE_TIMEOUT) do
+        run_shell("cd '#{REPOS_ROOT}' && git clone '#{repo_url}' #{repo_name}")
       end
       logger.info "Finished cloning the repo #{repo_url}."
-    rescue Exception => error
-      logger.error "Error cloning the repo #{repo_url}: #{error.message}"
+    rescue Timeout::Error => error
+      logger.error "Timed out while trying to clone '#{repo_url}'"
+    rescue StandardError => error
+      logger.error "Unable to clone the repo #{repo_url}"
+    ensure
       # Having an empty directory with a bad checkout is not good and can mess up various parts of Barkeep.
-      # Clean it up.
-      FileUtils.rm_rf(repo_path)
+      FileUtils.rm_rf(repo_path) if error
     end
   end
 end
--- test/integration/clone_new_repo_integration_test.rb
+++ test/integration/clone_new_repo_integration_test.rb
@@ -4,6 +4,7 @@ require "pathological"
 require "test/test_helper"
 require "test/integration_test_helper"
 require "resque_jobs/clone_new_repo"
+require "fileutils"
 
 class CloneNewRepoIntegrationTest < Scope::TestCase
   include IntegrationTestHelper
@@ -13,7 +14,7 @@ class CloneNewRepoIntegrationTest < Scope::TestCase
 
     # We could also clone this repo from git@github.com:ooyala/barkeep_integration_tests.git, but that takes
     # about 5 seconds. Cloning from the local disk is much faster.
-    @@test_repo_url = File.join(File.dirname(__FILE__), "../fixtures/test_git_repo")
+    @@test_repo_url = File.expand_path(File.join(File.dirname(__FILE__), "../fixtures/test_git_repo"))
 
     delete_repo(@@test_repo_name)
   end
@@ -25,12 +26,19 @@ class CloneNewRepoIntegrationTest < Scope::TestCase
   should "clone a new repo to the correct place on disk" do
     assert_equal false, File.exists?(repo_path(@@test_repo_name))
     CloneNewRepo.perform(@@test_repo_name, @@test_repo_url)
-    assert File.exists?(repo_path(@@test_repo_name))
+    assert File.exists?(repo_path(@@test_repo_name)), "repo should've been cloned, but it's not on disk."
 
     # Ensure it successfully completed a full clone (i.e. commits can now be read by Grit).
     first_commit = "65a0045e7ac5329d76e6644aa2fb427b78100a7b"
     grit = Grit::Repo.new(repo_path(@@test_repo_name))
     assert_equal 1, grit.commits(first_commit).size
+  end
+
+  should "if a repo cannot be cloned, no trace of it should be left on disk" do
+    repo_name = "bad_repo_integration_test"
+    FileUtils.rm_rf(repo_path(repo_name))
+    CloneNewRepo.perform(repo_name, "bad_repo/url")
+    assert_equal false, File.exists?(repo_path(repo_name))
   end
 
   def repo_path(repo_name) File.join(REPOS_ROOT, @@test_repo_name) end
