f8ac88413716cf2bb3b5b4cf6e027fe7b0571a94
Caleb Spare <cespare@gmail.com>
2012-05-03 10:33:52 -0700

    Monky-patch grit methods to force encoding when they enter our system

--- lib/grit_extensions.rb
+++ lib/grit_extensions.rb
@@ -1,12 +1,21 @@
 # Extensions that are monkey-patched into Grit for convenience.
 
+# NOTE(caleb): Several methods (Commit#message, Actor#name, Actor#email) are monkey-patched to force UTF-8
+# encoding. This is because grit will return ASCII-8BIT strings (essentially a binary byte sequence) for these
+# fields, and this causes problems because Ruby 1.9 won't let you concatenate these with UTF-8 strings unless
+# the ASCII-8BIT string has no byte values > 128.
+
 require "grit"
+require "methodchain"
 
 module Grit
   class Commit
     attr_accessor :repo_name
     def link() "/commits/#{@repo_name}/#{@id}" end
     def timestamp() date.to_i end
+
+    alias_method :message_original, :message
+    def message() message_original.then { force_encoding("utf-8") } end
   end
 
   class Repo
@@ -17,6 +26,10 @@ module Grit
 
   class Actor
     def user() @user ||= User.find(:email => self.email) end
+    alias_method :name_original, :name
+    def name() name_original.then { force_encoding("utf-8") } end
+    alias_method :email_original, :email
+    def email() email_original.then { force_encoding("utf-8") } end
 
     # The default to_s() for Actor only includes the author's name, not email.
     def display_string
