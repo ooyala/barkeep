31198ce1b5eb8cf3af067f9dcabcbbbe7d8a12aa
Jack Veenstra <veenstra@gmail.com>
2013-01-17 13:07:08 -0800

    Modified the migration that populates the author_id field to iterate over the db commits (instead of Grit).

--- migrations/20130116152425_populate_author_id_field.rb
+++ migrations/20130116152425_populate_author_id_field.rb
@@ -5,55 +5,48 @@ require "grit"
 
 # This migration populates the "author_id" field in the "commits" table.
 
-# This is the number of commits that we fetch at a time.
-PAGE_SIZE = 100
-
 Sequel.migration do
   up do
-    repos = DB[:git_repos].all
-
     # Fetch all the authors (expected to be small, say less than 1000) so that we don't have to do
     # an extra SQL query for every commit to check if the author email exists. Then create a mapping
     # from email -> author.id
     rows = DB[:authors].all
-    puts "Read #{rows.length} rows from the 'authors' table."
     authors = {}
     rows.each do |row|
       authors[row[:email]] = row[:id]
     end
 
-    # Create a mapping from commit sha -> author email
-    shas = Hash.new { |hash, key| hash[key] = {} }
-    repos.each do |repo|
-      grit_repo = Grit::Repo.new(repo[:path])
-      total = 0
-      num = 0
-      begin
-        commits = grit_repo.commits("master", PAGE_SIZE, total)
-        commits.each do |commit|
-          email = commit.author.email
-          author_id = authors[email]
-          if author_id.nil?
-            # This shouldn't happen. We should already have the author in our db. But if not,
-            # add the author to the db.
-            user = DB[:users].first(:email => email)
-            user_id = user[:id] if user
-            DB[:authors].insert(:email => email, :name => commit.author.name, :user_id => user_id)
+    # Fetch all the git repos (also small, less than 100) and create a mapping from
+    # git_repo_id -> Grit::Repo
+    repos = {}
+    DB[:git_repos].each { |row| repos[row[:id]] = Grit::Repo.new(row[:path]) }
 
-            # Get the author_id and add it to the hash.
-            author = DB[:authors].first(:email => email)
-            author_id = author[:id]
-            authors[email] = author_id
-          end
+    total_updates = 0
+    new_authors = 0
+    commits = DB[:commits].filter(:author_id => nil).all
+    commits.each do |row|
+      commit = repos[row[:git_repo_id]].commit(row[:sha])
+      next unless commit
+      email = commit.author.email
+      author_id = authors[email]
+      # If the author is not in our db, then add it.
+      if author_id.nil?
+        # Check if the same email exists in the users table.
+        user = DB[:users].first(:email => email)
+        user_id = user[:id] if user
+        DB[:authors].insert(:email => email, :name => commit.author.name, :user_id => user_id)
 
-          # Update the author_id field.
-          DB[:commits].filter(:sha => commit.sha).update(:author_id => author_id)
-        end
-        num = commits.length
-        total += num
-      end while num == PAGE_SIZE
-      puts "Processed #{total} commits from repo #{repo[:path]}."
+        # Get the author_id and add it to the hash.
+        author = DB[:authors].first(:email => email)
+        author_id = author[:id]
+        authors[email] = author_id
+        new_authors += 1
+      end
+      total_updates += 1
+      DB[:commits].filter(:id => row[:id]).update(:author_id => authors[email])
     end
+    puts "New authors: #{new_authors}"
+    puts "Updated commits: #{total_updates}"
   end
 
   # We don't need to undo this.
