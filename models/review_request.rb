7831667fd4be75f7ad01323fec59ef7d3b517c6b
Jack Veenstra <veenstra@gmail.com>
2013-01-19 22:28:20 -0800

    Refactor models/review_request.rb and add requests_from_me().

--- models/review_request.rb
+++ models/review_request.rb
@@ -3,16 +3,19 @@ class ReviewRequest < Sequel::Model
   many_to_one :requester_user, :class => User
   many_to_one :reviewer_user, :class => User
 
-  def self.commits_with_uncompleted_reviews(user_id)
-    uncompleted = ReviewRequest.filter(:reviewer_user_id => user_id, :completed_at => nil).
-      group_by(:commit_id).all
-    commits = uncompleted.map do |review|
+  def self.get_grit_commits(reviews)
+    grit_commits = reviews.map do |review|
       grit_commit = MetaRepo.instance.grit_commit(review.commit.git_repo.name, review.commit.sha)
       next unless grit_commit
       grit_commit
     end
-    commits.reject!(&:nil?)
-    commits
+    grit_commits.reject(&:nil?)
+  end
+
+  def self.commits_with_uncompleted_reviews(user_id)
+    uncompleted = ReviewRequest.filter(:reviewer_user_id => user_id, :completed_at => nil).
+      group_by(:commit_id).all
+    get_grit_commits(uncompleted)
   end
 
   def self.recently_reviewed_commits(user_id)
@@ -20,13 +23,14 @@ class ReviewRequest < Sequel::Model
         exclude(:completed_at => nil).
         group_by(:commit_id).
         reverse_order(:completed_at).limit(5).all
-    commits = recently_reviewed.map do |review|
-      grit_commit = MetaRepo.instance.grit_commit(review.commit.git_repo.name, review.commit.sha)
-      next unless grit_commit
-      grit_commit
-    end
-    commits.reject!(&:nil?)
-    commits
+    get_grit_commits(recently_reviewed)
+  end
+
+  def self.requests_from_me(user_id)
+    p ReviewRequest.filter(:requester_user_id => user_id, :completed_at => nil).group_by(:commit_id).sql
+    uncompleted = ReviewRequest.filter(:requester_user_id => user_id, :completed_at => nil).
+      group_by(:commit_id).all
+    get_grit_commits(uncompleted)
   end
 
   def self.complete_requests(commit_id)
