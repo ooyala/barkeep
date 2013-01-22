cfbbccde921129100a079e29ea02097cbe1394f6
Florian Unglaub <florian.unglaub@nix-wie-weg.de>
2013-01-22 16:08:41 +0100

    Make the redis and resque databases configurable to avoid issues with other applications that use redis/resque

--- lib/script_environment.rb
+++ lib/script_environment.rb
@@ -17,6 +17,9 @@ require "redis"
 require "lib/redis_manager"
 require "backtrace_shortener"
 
+Resque.redis = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT,
+                         :db => RESQUE_DB)
+
 # Make the developer experience better by shortening backtraces.
 BacktraceShortener.monkey_patch_the_exception_class! unless ENV["RACK_ENV"] == "production"
 
