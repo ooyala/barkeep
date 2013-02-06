271e2ac2374d399dd47019137583e8b62cb7de03
Kazuhiro Yamada <yamadakazu45@gmail.com>
2013-02-06 05:48:17 +0000

    Support send an email via :sendmail

--- environment.rb
+++ environment.rb
@@ -4,6 +4,9 @@ DB_NAME = "barkeep"
 DB_USER = "root"
 DB_PASSWORD = ""
 
+MAIL_DELIVERY_METHOD = "gmail"  # gmail or sendmail
+
+# Only if MAIL_DELIVERY_METHOD is "gmail", you must specify the following.
 # These are the credentials of the Gmail account that you want to send mail as.
 # NOTE(philc): We may want to make configuration variables which generically support SMTP.
 GMAIL_ADDRESS = ""
