98b2e86825b122e8b5689504c431bcb3b872ffa1
Caleb Spare <cespare@gmail.com>
2013-02-12 19:19:27 -0800

    Add a migration for adding the deleted_at column to users.

---
+++ migrations/20130212183244_add_deleted_column_to_users.rb
@@ -0,0 +1,9 @@
+require "bundler/setup"
+require "pathological"
+require "migrations/migration_helper.rb"
+
+Sequel.migration do
+  change do
+      alter_table(:users) { add_column :deleted_at, DateTime }
+  end
+end
