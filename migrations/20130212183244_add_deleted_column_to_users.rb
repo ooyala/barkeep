a6e0e72dc365506e666eb81d1bff0bd9ea1d5d00
Caleb Spare <cespare@gmail.com>
2013-02-14 02:21:05 -0800

    Indentation fix.

--- migrations/20130212183244_add_deleted_column_to_users.rb
+++ migrations/20130212183244_add_deleted_column_to_users.rb
@@ -4,6 +4,6 @@ require "migrations/migration_helper.rb"
 
 Sequel.migration do
   change do
-      alter_table(:users) { add_column :deleted_at, DateTime }
+    alter_table(:users) { add_column :deleted_at, DateTime }
   end
 end
