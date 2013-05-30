f860513f251a97f388df5a5f618b5367bc4039ab
Caleb Spare <cespare@gmail.com>
2013-05-30 13:06:26 -0700

    Only allow a single fetch_commits job at a time.
    Fixes #280.

--- script/run_resque.rb
+++ script/run_resque.rb
@@ -7,11 +7,10 @@ require "./environment.rb"
 # is critical because you don't want unimportant slow jobs to starve the faster jobs. Resque workers will
 # process jobs strictly in this order -- there is no fair round-robin among the queues.
 #
-# If we ever get the point where we want true concurrency (drain more than one slow queue at the same time),
-# then we should run two instances of `rake resque` via Foreman, each with a different set of queues.
+# The fetch_commits job is done separately by a dedicated queue (see below) because we only want to run one
+# instance at a time.
 queue_order = %W(
   db_commit_ingest
-  fetch_commits
   clone_new_repo
   delete_repo
   delete_old_comments_by_demo_users
@@ -22,16 +21,21 @@ queue_order = %W(
   generate_tagged_diffs
 )
 
-resque_job_files = Dir["resque_jobs/*.rb"].map { |file| File.basename(file).sub(/\.rb$/, "") }
-if Set.new(queue_order) != Set.new(resque_job_files)
+resque_job_files = Dir["resque_jobs/*.rb"].map { |file| File.basename(file).sub(/\.rb$/, "") }.reject
+if Set.new(queue_order) != (Set.new(resque_job_files) - ["fetch_commits"])
   abort "The ordered list of resque queues does not match the files in resque_jobs/!"
 end
 
-RESQUE_WORKERS.times do |i|
-  # In production, wait 10 seconds before spawning workers and in between workers to give the web workers a
-  # chance to start up quickly without contending for resources.
-  sleep 10 if defined?(RACK_ENV) && RACK_ENV == "production"
-  spawn({ "QUEUE" => queue_order.join(",") }, "bundle exec rake resque:work")
+def run_queues(queues, workers)
+  workers.times do |i|
+    # In production, wait 10 seconds before spawning workers and in between workers to give the web workers a
+    # chance to start up quickly without contending for resources.
+    sleep 10 if defined?(RACK_ENV) && RACK_ENV == "production"
+    spawn({ "QUEUE" => queues.join(",") }, "bundle exec rake resque:work")
+  end
 end
+
+run_queues(["fetch_commits"], 1)
+run_queues(queue_order, RESQUE_WORKERS)
 
 Process.waitall
