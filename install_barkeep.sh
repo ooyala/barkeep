2714e41b3c16beb980673b77cfbfd85da8297d57
Jakukyo Friel <weakish@gmail.com>
2016-08-28 13:11:59 +0800

    install_barkeep.sh: combine apt-get commands.

--- install_barkeep.sh
+++ install_barkeep.sh
@@ -15,12 +15,7 @@ set -e
 # Install core dependencies
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y g++ build-essential libxslt1-dev libxml2-dev \
-  python-dev libmysqlclient-dev redis-server mysql-server nginx
-
-# Install git 1.7.6+
-sudo apt-get -y install python-software-properties
-sudo add-apt-repository -y ppa:git-core/ppa && sudo apt-get update
-sudo apt-get install -y git
+  python-dev libmysqlclient-dev redis-server mysql-server nginx git
 
 # Install ruby 1.9.3-p194
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
